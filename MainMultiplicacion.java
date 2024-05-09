743f966bfd629fe32955e25bd3b23eb4a5e03305
alfonsosolis <ponchomoran7@gmail.com>
2024-05-08 23:36:55 -0600

    version 2

--- MainMultiplicacion.java
+++ MainMultiplicacion.java
@@ -14,9 +14,9 @@ public class MainMultiplicacion {
         y = op.nextInt();
 
         Multiplicacion evaluar = new Multiplicacion();
-        evaluar.x = x;
-        evaluar.y = y;
-        evaluar.mostrarResultado();
+        evaluar.setX(x);
+        evaluar.setY(y);
+        System.out.println("EL RESULTADO DE LA MULTIPLICACION ES: "+evaluar.mostrarResultado());
     }
 
 }
